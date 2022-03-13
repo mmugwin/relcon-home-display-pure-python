62ec90a0691571367390b63ed7de29c0d48af774
mmugwin <ngonimugwisi@gmail.com>
2022-03-13 17:58:47 +0000

    error handling and whatnot

--- home_disp_pygame.py
+++ home_disp_pygame.py
@@ -1,49 +1,42 @@
 import pygame
 import random
 import time
+import serial
+import json
 from math import radians, sin, cos
 
 pygame.init()
 screen_width = 640
 screen_height = 480
-# screen = pygame.display.set_mode((screen_width, screen_height))
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("RELCON InHome Display")
 
-wifi_icon = pygame.image.load("./images/outline_wifi_black_24dp.png")
+path = "/home/pi/Desktop/relcon-home-display-pure-python"
+
+wifi_icon = pygame.image.load(path+"/images/outline_wifi_black_24dp.png")
 wifi_icon_x = 90
 wifi_icon_y = 20
 
-batt_icon = pygame.image.load("./images/battery_charging_full_black_24dp.svg")
+batt_icon = pygame.image.load(path+"/images/battery_charging_full_black_24dp.svg")
 batt_icon_x = 30
 batt_icon_y = 20
 
-background_img = pygame.image.load("./images/background.svg")
+background_img = pygame.image.load(path+"/images/background.svg")
 background_img = pygame.transform.scale(background_img, (screen_width, screen_height))
 background_x = -5
 background_y = 0
+
 pygame.font.get_fonts()
 ['arial', 'arialblack', 'bahnschrift']
-
-# header_font = pygame.font.SysFont('bahnschrift', 35)
-# usage_font = pygame.font.SysFont('bahnschrift', 45)
-# footer_font = pygame.font.SysFont('bahnschrift', 35)
-# curr_power_font = pygame.font.SysFont('bahnschrift', 60)
 
 header_font = pygame.font.SysFont('dejavusansmono', 35)
 usage_font = pygame.font.SysFont('dejavusansmono', 47)
 footer_font = pygame.font.SysFont('dejavusansmono', 30)
 curr_power_font = pygame.font.SysFont('liberationmono', 60)
 
+##########################
 title = "RELCON Homebox #"
-box_num = 4
-
-######
-batt_status = 1 
-connection_status = 1
-
-
-######
+##########################
 
 def render_icons(batt_status, connection_status):
     if batt_status:
@@ -59,7 +52,7 @@ def render_icons(batt_status, connection_status):
     screen.blit(wifi_icon, (wifi_icon_x, wifi_icon_y))
     screen.blit(batt_icon, (batt_icon_x, batt_icon_y))
 
-def render_text():
+def render_text(box_num):
     header_x = 180
     header_y = 24
     usage_x = 250
@@ -75,13 +68,13 @@ def render_text():
     screen.blit(footer_tag, (footer_x, footer_y))
     screen.blit(header, (header_x, header_y))
 
-def drawArc(display, startAngle, endAngle, distance, pos, color, thickness=1):
-    if startAngle > endAngle:
-        theta = endAngle
-        bigger = startAngle
+def draw_arc(display, start_angle, end_angle, distance, pos, color, thickness = 1):
+    if start_angle > end_angle:
+        theta = end_angle
+        bigger = start_angle
     else:
-        theta = startAngle
-        bigger = endAngle
+        theta = start_angle
+        bigger = end_angle
         
     while theta < bigger: 
         for t in range(thickness):
@@ -90,51 +83,97 @@ def drawArc(display, startAngle, endAngle, distance, pos, color, thickness=1):
             display.set_at((x, y), color)
             theta += 0.01
 
-def update_data():
-    # this is where we read the serial line in a try catch
-    # we then pass the values to QML
-    
-    # ser = serial.Serial('/dev/ttyS0',
-    #                     baudrate = 9600)
-    # data = ser.read_until(b'\n').decode('ascii')
-    # data =  list(data)
-    # data = ''.join(data)
-    # data = json.loads(data)
+def check_keys(data):
+    keys = ['"curr_power"', '"link_status"', '"conn"', '"box_num"', '"batt_percent"']
+    for key in keys:
+        if key not in data:
+            print(key)
+            return False
+    return True
 
-    # curr_power_usage = data['curr_power']
-    # connection_status = data['link_status']
-    # box_num = data['box_num']
-    # curr_time = data['curr_time']
+def check_val_type(data):
+    keys = {"curr_power": "<class 'float'>", "link_status": "<class 'int'>", "conn": "<class 'int'>", "box_num": "<class 'int'>", "batt_percent": "<class 'float'>"}
+    for key in keys:
+        tmp = str(type(data[key]))
+        if tmp != keys[key]:
+            print('bad val')
+            return False
+    return True
 
-    # temporary hardcoded data for demo
-    power_limit = 300
-    i = random.randint(1,power_limit)
-    curr_power_usage = int(i/power_limit * 270)
-    connection_status = 0
-    box_num = 4
-
-    if i > 240:
-        color = (200, 0, 0)
-    elif i > 200:
-        color = (255, 191, 0)
+def check_power_status(curr_power_usage):
+    power_status = []
+    if curr_power_usage > 270:
+        power_status.append((200, 0, 0))
+    elif curr_power_usage > 220:
+        power_status.append((255, 191, 0))
     else:
-        color = (0, 128, 0)
+        power_status.append((0, 128, 0))
 
-    if i < 100:
+    if curr_power_usage < 100:
         curr_power_x = 250
-    elif i < 200:
+    elif curr_power_usage < 200:
         curr_power_x = 230
     else:
-        curr_power_x = 230
-    
-    curr_power_y = 250
-    
-    curr_power_tag = curr_power_font.render(str(i) + " W", True, (255, 255, 255))
-    screen.blit(curr_power_tag, (curr_power_x, curr_power_y))
+        curr_power_x = 230  
+    power_status.append(curr_power_x)
+    return power_status
 
-    drawArc(screen, 225, 225-curr_power_usage, 120, [screen_width/2, screen_height/2 + 40 + 10], color, thickness = 15)
+def update_data():
+    power_limit = 300
+    try:
+        ser = serial.Serial('/dev/ttyS0',
+                            baudrate = 9600)
+        data = ser.read_until(b'\n').decode('ascii', 'ignore')
+        print(data)
+
+        if check_keys(data) == False:
+            ser.close()
+            return
+
+        data =  list(data)
+        data = ''.join(data)
+        data = json.loads(data)
+
+        if check_val_type(data) == False:
+            ser.close()
+            return
+
+        curr_power_usage = data['curr_power']
+        power_status = check_power_status(curr_power_usage)
+        comms_connection = data['link_status']
+        hub_connection = data['conn']
+        box_num = data['box_num']
+        render_text(box_num)
+        render_icons(hub_connection, comms_connection)
+
+        color = power_status[0]    
+        curr_power_x = power_status[1]
+        curr_power_y = 250    
+        curr_power_tag = curr_power_font.render(str(curr_power_usage) + " W", True, (255, 255, 255))
+        screen.blit(curr_power_tag, (curr_power_x, curr_power_y))
+        draw_arc(screen, 225, 225-curr_power_usage, 120, [screen_width/2, screen_height/2 + 40 + 10], color, thickness = 15)      
+
+        ser.close()
+
+    except:
+        curr_power_usage = random.randint(1, power_limit)
+        curr_power_usage = int(curr_power_usage/power_limit * 270)
+        power_status = check_power_status(curr_power_usage)
+        connection_status = 0
+        box_num = 2
+
+        comms_connection = 0
+        hub_connection = 0
+        render_text(box_num)
+        render_icons(hub_connection, comms_connection)
+
+        color = power_status[0]    
+        curr_power_x = power_status[1]
+        curr_power_y = 250    
+        curr_power_tag = curr_power_font.render(str(curr_power_usage) + " W", True, (255, 255, 255))
+        screen.blit(curr_power_tag, (curr_power_x, curr_power_y))
+        draw_arc(screen, 225, 225-curr_power_usage, 120, [screen_width/2, screen_height/2 + 40 + 10], color, thickness = 15)
    
-
 running = True
 while running:
     screen.fill((255, 255, 255))
@@ -143,9 +182,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    render_icons(batt_status, connection_status)
-    render_text()
-    drawArc(screen, 225, -45, 120, [screen_width/2, screen_height/2 + 40 + 10], (72, 72, 72), thickness = 15)
+    draw_arc(screen, 225, -45, 120, [screen_width/2, screen_height/2 + 40 + 10], (72, 72, 72), thickness = 15)
     update_data()
     pygame.display.update()
     time.sleep(4)
